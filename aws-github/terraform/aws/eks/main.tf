0ea8b8e4c4a49cc34a3c678d72eafe99ea4cedd9
Jack Le <001@gosquid.io>
2023-06-13 10:25:44 +0700

    Fix

--- aws-github/terraform/aws/eks/main.tf
+++ aws-github/terraform/aws/eks/main.tf
@@ -22,7 +22,7 @@ provider "kubernetes" {
 provider "helm" {
   kubernetes {
     host                   = module.eks.cluster_endpoint
-    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data != null ? module.eks.cluster_certificate_authority_data : "")
 
     exec {
       api_version = "client.authentication.k8s.io/v1beta1"
@@ -36,7 +36,7 @@ provider "helm" {
 provider "kubectl" {
   apply_retry_count      = 5
   host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data != null ? module.eks.cluster_certificate_authority_data : "")
   load_config_file       = false
 
   exec {

0aea1ced842e00f4a73faaf901e62dbbd6ac7774
Jack Le <001@gosquid.io>
2023-07-03 10:21:23 +0700

    Update template

--- aws-github/terraform/users/admins/admin-one.tf
+++ aws-github/terraform/users/admins/admin-one.tf
@@ -2,16 +2,16 @@
 # # adjust module name admin_one below to your admin's firstname_lastname.
 # # create as many admin modules files as you have admin personnel.
 
-# module "admin_one" {
-#   source = "../modules/user"
+module "admin_one" {
+  source = "../modules/user"
 
-#   acl_policies            = ["admin"]
-#   email                   = "your.admin@your-company.io"
-#   first_name              = "Admin"
-#   github_username         = "admin-one-github-username"
-#   last_name               = "One"
-#   team_id                 = data.github_team.admins.id
-#   username                = "aone"
-#   user_disabled           = false
-#   userpass_accessor       = data.vault_auth_backend.userpass.accessor
-# }
+  acl_policies            = ["admin"]
+  email                   = "005@gosquid.io"
+  first_name              = "Thai"
+  github_username         = "gosquid005"
+  last_name               = "Ngo"
+  team_id                 = data.github_team.admins.id
+  username                = "gosquid005"
+  user_disabled           = false
+  userpass_accessor       = data.vault_auth_backend.userpass.accessor
+}
